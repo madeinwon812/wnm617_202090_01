ebff8704e45c1ba99ba7d5b7e367dc96e4af6348
madeinwon812 <70725742+madeinwon812@users.noreply.github.com>
2020-10-22 13:06:51 -0700

    Add files via upload

--- jiwon.lee/js/signin.js
+++ jiwon.lee/js/signin.js
@@ -1,61 +1,36 @@
+const checkSigninForm = () => {
+   let user = $("#signin-username").val();
+   let pass = $("#signin-password").val();
 
-const checkSigninForm = async() => {
-	let user = $("#signin-username").val();
-	let pass = $("#signin-password").val();
+   console.log(user,pass)
 
-	console.log(user,pass);
+   if(user == 'user' && pass == 'pass') {
+      // logged in
+      console.log("success");
+      sessionStorage.userId = 3;
+   } else {
+      // not logged in
+      console.log("failure");
+      sessionStorage.removeItem('userId');
 
-	let found_user = await query({
-		type:"check_signin",
-		params:[user,pass],
-	})
+      // do something here
 
-	if(found_user.result.length > 0) {
-		// logged in
-		console.log("success");
-		sessionStorage.userId = found_user.result[0].id;
-		$("#signin-form")[0].reset();
-	} else {
-		// not logged in
-		console.log("failure");
-		sessionStorage.removeItem('userId');
-		
-	}
+   }
 
-	checkUserId();
-};
+   checkUserId();
+}
+
 
 const checkUserId = () => {
-	let p = ["#signin-page","#signup-page",""];
+   let p = ['#signin-page','#signup-page',''];
 
-	if(sessionStorage.userId===undefined) {
-		// not logged in
-		if(!p.some(o=>window.location.hash===o))
-			$.mobile.navigate("#signin-page");
-	} else {
-		// logged in
-		if(p.some(o=>window.location.hash===o))
-			$.mobile.navigate("#recent-page");
-	}
-};
-
-const checkSignupForm = () => {
-	let username = $("#signup-username").val();
-	let email = $("#signup-email").val();
-	let password = $("#signup-password").val();
-	let password2 = $("#signup-password2").val();
-
-	if(password!=password2) {
-		throw "Passwords don't match";
-		return;
-	} else {
-		query({
-			type:'insert_user',
-			params:[username,email,password]
-		}).then(d=>{
-			if(d.error) throw d.error;
-			// d.result == new user id
-			$.mobile.navigate("#signin-page");
-		});
-	}
+   if(sessionStorage.userId === undefined) {
+      // not logged in
+      if(!p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#signin-page");
+   } else {
+      // logged in
+      if(p.some(o=>window.location.hash===o))
+         $.mobile.navigate("#recent-page");
+   }
 }
